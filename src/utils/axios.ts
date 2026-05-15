e44259495d8c8db32212d941ea6c961eede413a7
xushook <xushook@gmail.com>
2026-05-15 20:08:25 +0800

    Add files via upload

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -9,6 +9,7 @@ import axios from 'axios'
 import config from './../config'
 import { ElMessage } from 'element-plus'
 import router from './../router'
+import Api from './../api'
 
 // console.log('router=>', router)
 // console.log('config=>', config)
@@ -22,6 +23,10 @@ const service = axios.create({
 // 添加请求拦截器
 service.interceptors.request.use((config) => {
     // 在发送请求之前做些什么
+    let data = window.sessionStorage.getItem('userinfo') || '{}'
+    let { token = '' } = JSON.parse(data)
+    let header = config.headers
+    if (!header.Authorization) header.Authorization = 'Bearer ' + token
     return config;
 }, function (error) {
     // 对请求错误做些什么
@@ -29,33 +34,55 @@ service.interceptors.request.use((config) => {
 });
 
 // 添加响应拦截器
+let isRefreshing = false
 service.interceptors.response.use((res: any) => {
+    if (!res.data.code) {
+        return res.data
+    }
     console.log('res',res)
     // 2xx 范围内的状态码都会触发该函数。
     // 对响应数据做点什么
-    const { data, msg, statusCode } = res.data
-    if (statusCode == 200) {
+    const { data, msg, code } = res.data
+    if (code == 200) {
         return data
-    } else if (statusCode == 501) {
+    } else if (code == 501) {
         ElMessage({
             message: TOKEN_INVALID,
             type: 'error'
         })
         router.push('./login')
         return Promise.reject(TOKEN_INVALID)
-    } else if (statusCode == 401) {
-        ElMessage({
-            message: NETWORK_ERROR,
-            type: 'error'
-        })
-        return Promise.reject(NETWORK_ERROR)
+    } else if (code == 401) {
+        if (!isRefreshing) {
+            isRefreshing = true
+            ElMessage({
+                message: msg,
+                type: 'error',
+            })
+            // 获取本地缓存中的RefreshToken
+            const { refreshToken } = JSON.parse(sessionStorage.getItem('userinfo') || '{}')
+            // 请求新的token
+            console.log('refreshToken', typeof refreshToken);
+            // let ress = await Api.refreshToken({ refreshToken })
+            console.log(Api);
+            
+            isRefreshing = false
+        }
+        // 请求成功，开启刷新标识
+        // localStorage.setItem('AccessToken', res.accessToken)
+        // localStorage.setItem('RefreshToken', res.refreshToken)
+        // // 已经刷新了token，将所有队列中的请求进行重试
+        // requests.forEach((item) => item(res?.accessToken))
+        // requests = []
+        // router.push('/login')
+        return Promise.reject(msg)
     } else {
         ElMessage({
             message: msg,
             type: 'error'
         })
         return Promise.reject(msg)
-    }
+    } 
 }, function (error) {
     // 超出 2xx 范围的状态码都会触发该函数。
     // 对响应错误做点什么
